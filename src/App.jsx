496f7bfb904bb1ed516e4e9de5b6aa63652e6f29
jaen <jaenfigueroa03042000@gmail.com>
2023-02-08 15:35:10 -0500

    maquetacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,108 @@
-
-import { Header } from "./layout/Header"
+import './App.css'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
+
+
+
+
+
+  //////////////////////////////////////////////////
   return (
-    <div className="bg-gray-900"> 
-      <Header  />
-    </div>
+
+    <>
+
+      <div className="app"> 
+
+        {/* MAIN */}
+        <main className='main'>
+
+          {/* SECCION 1 */}
+          <section>
+            <div className='slider'></div>
+
+            <div className='caja'>
+
+              <h2 className='caja__titulo'>Productos</h2>
+
+              <div className='caja__contenedor'>
+                <article>
+                  <h2>calzado Hombre random</h2>
+                  <img className='card__imagen'/>
+                  <button>
+                    <i class="fa-solid fa-chevron-left"></i>
+                  </button>
+                </article>
+
+                <article>
+                  <h2>calzado Hombre random</h2>
+                  <img className='card__imagen'/>
+                  <button>
+                    <i class="fa-solid fa-chevron-left"></i>
+                  </button>
+                </article>
+
+                <article>
+                  <h2>calzado Hombre random</h2>
+                  <img className='card__imagen'/>
+                  <button>
+                    <i class="fa-solid fa-chevron-left"></i>
+                  </button>
+                </article>
+
+              </div>
+
+            </div>
+          </section>
+
+          {/* SECCION 2*/}
+          <section>
+            
+          </section>
+
+          {/* SECCION 3*/}
+          <section>
+          
+          </section>
+
+          
+          {/* SECCION 4*/}
+          <section>
+          
+          </section>
+
+        </main>
+
+
+
+
+
+
+        {/* ASIDE */}
+        <aside className='aside'>
+
+          <div>
+            <i class="fa-solid fa-earth-americas"></i>
+            <i class="fa-solid fa-earth-americas"></i>
+            <i class="fa-solid fa-earth-americas"></i>
+            <i class="fa-solid fa-earth-americas"></i>
+          </div>
+
+          <div>
+            <i class="fa-solid fa-earth-americas"></i>
+            <i class="fa-solid fa-earth-americas"></i>
+            <i class="fa-solid fa-earth-americas"></i>
+            <i class="fa-solid fa-earth-americas"></i>
+          </div>
+        </aside>
+
+
+      </div>
+
+
+      {/* FOOTER */}
+      <Footer/>
+    </>
   )
 }
 
